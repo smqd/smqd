555520bf927df3bbe7ad5e75e2a2100e3251a001
Kwon, Yeong-Eon <eirny@uangel.com>
2018-07-23 14:40:06 +0900

    start/stop script

--- bin/start-smqd.sh
+++ bin/start-smqd.sh
@@ -5,15 +5,35 @@ SMQD_ENV="$SMQD_ENV -Dlogback.configurationFile=${SMQD_HOME_DIR}/conf/logback.xm
 SMQD_ENV="$SMQD_ENV -Dsmqd.plugin.dir=${SMQD_HOME_DIR}/plugins"
 SMQD_ENV="$SMQD_ENV -Dsmqd.plugin.manifest=${SMQD_HOME_DIR}/conf/plugin-manifest.conf"
 
-SMQD_CMD='nohup ${SMQD_HOME_DIR}/bin/smqd ${SMQD_ENV} > /dev/null 2>&1 & echo $! > ${SMQD_HOME_DIR}/bin/smqd.pid'
+display_usage() {
+  echo "Usage: start-smqd.sh [-f|-d|-h]"
+  echo "       -h   display usage"
+  echo "       -f   foreground"
+  echo "       -d   background"
+}
 
-while getopts "f" option
+if [ $# -eq 0 ]
+then
+  display_usage
+  exit 0
+fi
+
+while getopts "fdh" option
 do
   case "${option}" in
     f)
       SMQD_CMD='${SMQD_HOME_DIR}/bin/smqd ${SMQD_ENV}'
       ;;
+    d)
+      SMQD_CMD='nohup ${SMQD_HOME_DIR}/bin/smqd ${SMQD_ENV} > /dev/null 2>&1 & echo $! > ${SMQD_HOME_DIR}/bin/smqd.pid'
+      ;;
+    h)
+      display_usage
+      exit 0
+      ;;
     *)
+      display_usage
+      exit 0
       ;;
   esac
 done
--- bin/stop-smqd.sh
+++ bin/stop-smqd.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-kill `cat ${SMQD_HOME_DIR}/bin/smqd.pid`
+kill `cat ${SMQD_HOME_DIR}/bin/smqd.pid` && rm ${SMQD_HOME_DIR}/bin/smqd.pid
